3dd0bb83b2bdc7b53c66afce512f8365267e2a98
DComrade <igoncharovdanil@gmail.com>
2021-11-15 14:05:19 +0700

    Preparation Project.

---
+++ CollectionView/Sorces/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  CollectionView
+//
+//  Created by Danil Goncharov on 15.11.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
