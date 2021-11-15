c83f37f4aa973c37b7fcd3ce26aafb151f233eaf
DComrade <igoncharovdanil@gmail.com>
2021-11-15 17:04:19 +0700

    Add TabBarController.

---
+++ CollectionView/Sorces/Scenes/MediaViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MediaViewController.swift
+//  CollectionView
+//
+//  Created by Danil Goncharov on 15.11.2021.
+//
+
+import UIKit
+
+class MediaViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        title = "Медиатека"
+    }
+}
+
---
+++ CollectionView/Sorces/Scenes/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  CollectionView
+//
+//  Created by Danil Goncharov on 15.11.2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        title = "Поиск"
+    }
+}
+
